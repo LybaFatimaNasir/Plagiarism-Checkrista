3d3e3b75cccb953c3599f50af32e982b55784c22
Bareerah Muslim <biyamuslim@gmail.com>
2020-07-20 02:54:17 +0500

    changes

--- checkerista/checker/checker/settings.py
+++ checkerista/checker/checker/settings.py
@@ -121,5 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'STATIC'),
+    os.path.join(BASE_DIR,'static'),
+    
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
