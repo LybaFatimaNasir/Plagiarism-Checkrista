0b5dcacb9b6207fb3767c8103b25d9fb139ee9d9
LybaFatimanasir <lf.nasir689@gmail.com>
2020-07-28 16:47:02 +0500

    another update

--- checkerista/checker/tool/admin.py
+++ checkerista/checker/tool/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import text
+from .models import text,compare
 # Register your models here.
 admin.site.register(text)
--- checkerista/checker/tool/models.py
+++ checkerista/checker/tool/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class text(models.Model):
     Content = models.TextField()
-    #text_id = models.ForeignKey(text, on_delete=models.CASCADE)
 
+    #text_id = models.ForeignKey(text, on_delete=models.CASCADE)
+class compare(models.Model):
+    comparison= models.TextField()
 
 #class url(models.Model):
  #  Content= models.URLField()
