3b76e405159ba3e1ea338dcd33c38b2063508911
LybaFatimanasir <lf.nasir689@gmail.com>
2020-08-15 20:19:26 +0500

    🚀Modified

--- checkerista/checker/tool/views.py
+++ checkerista/checker/tool/views.py
@@ -4,46 +4,34 @@ from django.http import HttpResponseRedirect
 from .models import text
 
 def results(request):
-        # try:
+
+    objCount = text.objects.all() #all  the objects present
+    
+    stu = {
+        'all_Is' : objCount
+    }
+
+ 
+
         
-        content = request.POST.get('content')
-       # content.save()
-            # newItem =  text(Content=request.POST['newItem'])
-            # 
-        # except KeyError:
-            # newItem = "No value"
-            # result = request.POST.get('result')
-        print(content)
-        stuff_for_frontend = {
-                'content' : content,
-            }
-            
-        return render(request,'results.html', stuff_for_frontend)
-    # objCount = text.objects.all() #count of all  the objects present
-      
-    # stu = {
-        # 'all_Is' : objCount
-    # }
 
     
-    #for p in text.objects.raw('Select * from tool_text'):
-     #print (p)
+
+    return render(request,'results.html', stu)
+
     #{'res' : stu}
-    #return render (request,'result.html')
-
-
 
 
 def index(request):
     return render(request,'tool.html')
 
-# def addText(request):
-    # try:
-        # newItem =  text(Content=request.POST['Content'])
-        # newItem.save()
-    # except KeyError:
-        # newItem = "Guest"
-    # return HttpResponseRedirect('/tool/' )
+def addText(request):
+    try:
+        newItem =  text(Content=request.POST['Content'])
+        newItem.save()
+    except KeyError:
+        newItem = "Guest"
+    return HttpResponseRedirect('/tool/' )
 
 
 ##########
